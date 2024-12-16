ae897e1ff4490e04547a2cbfcdb52b0ac4daa264
phanirithvij <phanirithvij2000@gmail.com>
2024-12-16 16:04:39 +0530

    re-init tree
    reason: the repo had merge commits, which made it hard to develop
    as of now this is a fresh start, no more merge commits from now

---
+++ scripts/build.ps1
@@ -0,0 +1,53 @@
+Set-PSDebug -Trace 1
+
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue dist
+Copy-Item -Recurse src -Destination dist
+
+if (-Not (Get-Command minify -errorAction SilentlyContinue)) {
+    go install github.com/tdewolff/minify/cmd/minify@latest
+}
+
+$content = (Get-Content -Path dist\js\common.js)
+# multiline regex with (?ms) or whatnot is not working at all
+# so split them to two replaces one per match
+# can't comment between ` escaped lines
+$content `
+    -replace 'const DEVELOPMENT_FETCH_REQS=(true|false)', '' | `
+    Out-File -Encoding utf8 -FilePath dist\js\common.js
+    # -replace 'DEVELOPMENT=true', 'DEVELOPMENT=false' `
+    # -replace 'const devLoggerSetup=\(.*', 'const devloggerSetup=_=>_=>{};' `
+    # -replace 'return Function.prototype.bind.*;};$', '' | `
+
+# prevent tests from bundling
+Remove-Item -Recurse -ErrorAction SilentlyContinue dist\tests
+Remove-Item -ErrorAction SilentlyContinue dist\js\vendor\quint-2.23.1.js
+Remove-Item -ErrorAction SilentlyContinue dist\css\vendor\quint-2.18.2-dark.css
+
+if (-Not (Test-Path dist\js\background\env.js) ) {
+    $content = @"
+const SimklClientID = "$env:SIMKL_CLIENT_ID";
+const SimklClientSecret = "$env:SIMKL_CLIENT_SECRET";
+"@
+    $content | Out-File -Encoding utf8 -FilePath dist\js\background\env.js
+}
+
+minify -r -o dist/ src
+
+# validate generated js files for syntax
+Set-PSDebug -Trace 0
+
+$env:NO_COLOR = 1
+Get-ChildItem -Path dist -Filter *.js -Recurse | Foreach-Object {
+    Write-Host "node -c $($_.FullName)"
+    # (Get-Content $_.FullName) `
+    #     -replace "consoledebug\([^;]*\)\(\);", ";" `
+    #     -replace "console.debug\([^;]*\);", ";" | `
+    #     Out-File -Encoding utf8 -FilePath $_.FullName
+    minify -o $_.FullName $_.FullName
+    node -c $_.FullName
+    if (-Not $?) {
+        exit $?
+    }
+}
+
+Set-PSDebug -Trace 0
